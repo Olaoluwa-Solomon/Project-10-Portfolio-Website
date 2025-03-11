0283ab329052dafb85a0a720b3cc8d058210a3d5
zion-el <mchladelola@gmail.com>
2025-03-11 03:19:13 +0100

    new push

--- main.js
+++ main.js
@@ -25,6 +25,7 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 if (toggleBtn) {
   toggleBtn.addEventListener("click", linkAction);
 }
+
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll("section[id]");
 
